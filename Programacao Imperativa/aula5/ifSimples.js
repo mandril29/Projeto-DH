f3d7b6eabfacc1d80723d8c54227378b9fd922bd
Juani <alcalajuanignacio@hotmail.compwdcdgit --config user.name juani.agit innitgit initgit --versioncd ..clear>
2021-06-07 14:22:25 -0300

    Atualizando2

---
+++ Programacao Imperativa/aula5/ifSimples.js
@@ -0,0 +1,14 @@
+let numero = 10;
+numero = 20;
+
+if (numero>=15) {
+    //comandos que vamos a hacer en el caso que el numero sea mayor o igual a 15
+    console.log('O numero e maior o igual a 15');
+}
+
+else {
+    //comandos que vamos a hacer en el caso que el numero sea menor a 15
+    console.log('O numero e menor a 15');
+}
+
+
